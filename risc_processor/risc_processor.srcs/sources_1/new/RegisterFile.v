e2ca59b3abd5095c4ec9ecf20ab7da98aacca5fd
Aaditya Singh <126668725+kitkat1424@users.noreply.github.com>
2024-10-14 15:30:41 +0530

    Update RegisterFile.v

--- risc_processor/risc_processor.srcs/sources_1/new/RegisterFile.v
+++ risc_processor/risc_processor.srcs/sources_1/new/RegisterFile.v
@@ -19,7 +19,12 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module RegisterFile(input clk, regWrite, input [3:0] readReg1, readReg2, writeReg, input [15:0] writeData, output [15:0] readData1, readData2);
+module RegisterFile(
+    input clk, regWrite, 
+    input [3:0] readReg1, readReg2, writeReg, 
+    input [15:0] writeData, 
+    output [15:0] readData1, readData2
+);
     reg [15:0] registers[0:15];  // 16 registers
 
     assign readData1 = registers[readReg1];
